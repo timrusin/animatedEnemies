6de0ecd80f48e023dbfefb6520ef59ede5bcf1f5
timrusin <timrusin@gmail.com>
2022-08-29 16:51:59 -0500

    basic animation setup with two enemies sing js class

---
+++ script.js
@@ -0,0 +1,35 @@
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEGIHT = canvas.height = 1000;
+
+// enemy1 = {
+//     x: 10,
+//     y: 50,
+//     width: 200,
+//     height: 200,
+// }
+
+class Enemy {
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+
+    }
+};
+
+const enemy1 = new Enemy();
+const enemy2 = new Enemy()
+
+function animate() {
+    ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEGIHT)
+    enemy1.x++
+    enemy2.y++
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height)
+    ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height)
+    requestAnimationFrame(animate);
+}
+animate()
