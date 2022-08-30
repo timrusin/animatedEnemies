752a2418a02ca9f524c2002b13cdb762e184e419
timrusin <timrusin@gmail.com>
2022-08-29 20:50:14 -0500

    trying to get sliders to work

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
 /**@type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d');
-CANVAS_WIDTH = canvas.width = 500;
-CANVAS_HEGIHT = canvas.height = 1000;
-const numberOfEnemies = 10
-const enemiesArray = []
+CANVAS_WIDTH = canvas.width = 250;
+CANVAS_HEGIHT = canvas.height = 500;
+let numberOfEnemies = 10;
+let enemySize = 3;
 gameFrame = 0;
+const enemiesArray = [];
 
-class Enemy {
-    constructor(){
-        this.image = new Image();
-        this.image.src = './enemies/enemy1.png'
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
-        this.width = this.spriteWidth / 2.5;
-        this.height = this.spriteHeight / 2.5;
-        this.x = Math.random() * (canvas.width - this.width);
-        this.y = Math.random() * (canvas.height - this.height);
-        this.frame = 0;
-        this.flapSpeed = Math.floor(Math.random() * 3 + 1)
-    }
-    update(){
-        this.x += Math.random() * 3 - 1.5;
-        this.y += Math.random() * 3 - 1.5;
-        // animate sprites
-        if (gameFrame % this.flapSpeed === 0){
-            this.frame > 4 ? this.frame = 0 : this.frame++;
+    const numberSlider = document.getElementById('numberSlider')
+    numberSlider.value = numberOfEnemies;
+    const showNumber = document.getElementById('showNumber')
+    showNumber.innerHTML = numberOfEnemies;
+    numberSlider.addEventListener('change', function(e){
+        numberOfEnemies = e.target.value;
+        showNumber.innerHTML = e.target.value;
+    })
+    
+    
+    const sizeSlider = document.getElementById('sizeSlider')
+    sizeSlider.value = enemySize;
+    const showSize = document.getElementById('showSize')
+    showSize.innerHTML = enemySize
+    sizeSlider.addEventListener('change', function(e){
+        enemySize = e.target.value;
+        showSize.innerHTML = e.target.value;
+    })
+    
+    class Enemy {
+        constructor(){
+            this.image = new Image();
+            this.image.src = './enemies/enemy1.png'
+            this.spriteWidth = 293;
+            this.spriteHeight = 155;
+            this.width = this.spriteWidth / enemySize;
+            this.height = this.spriteHeight / enemySize;
+            this.x = Math.random() * (CANVAS_WIDTH - this.width);
+            this.y = Math.random() * (CANVAS_HEGIHT - this.height);
+            this.frame = 0;
+            this.flapSpeed = Math.floor(Math.random() * 2 -4)
         }
+        update(){
+            this.x += Math.random() * 3 - 1.5;
+            this.y += Math.random() * 3 - 1.5;
+            // animate sprites
+            if (gameFrame % this.flapSpeed === 0){
+                this.frame > 4 ? this.frame = 0 : this.frame++;
+            }
+        }
+        draw(){
+            ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        }
+    };
+    for (let i = 0; i < numberOfEnemies; i++){
+        enemiesArray.push(new Enemy());
     }
-    draw(){
-        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
-    }
-};
+    
+    function animate() {
+        ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEGIHT);
+        enemiesArray.forEach(enemy => {
+                enemy.draw()
+                enemy.update()
+            });
+            gameFrame ++;
+            requestAnimationFrame(animate);
+        }
+    animate()
 
-for (let i = 0; i < numberOfEnemies; i++){
-    enemiesArray.push(new Enemy());
-}
 
-function animate() {
-    ctx.clearRect(0,0,CANVAS_WIDTH, CANVAS_HEGIHT);
-    enemiesArray.forEach(enemy => {
-            enemy.draw()
-            enemy.update()
-        });
-        gameFrame ++;
-        requestAnimationFrame(animate);
-    }
-animate()
